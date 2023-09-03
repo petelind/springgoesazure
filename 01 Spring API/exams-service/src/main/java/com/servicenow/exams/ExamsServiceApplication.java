11e664a6be656eeee35ef82a907b82243f97ddfc
petelind <denis@nyu.edu>
2023-09-02 22:56:08 -0500

    feat(swagger): add openapi support

--- 01 Spring API/exams-service/src/main/java/com/servicenow/exams/ExamsServiceApplication.java
+++ 01 Spring API/exams-service/src/main/java/com/servicenow/exams/ExamsServiceApplication.java
@@ -1,9 +1,11 @@
 package com.servicenow.exams;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class ExamsServiceApplication {
 
     public static void main(String[] args) {
