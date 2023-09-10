4b71f13f34641f220290d13909979e7d9d42f991
petelind <denis@nyu.edu>
2023-09-10 17:18:32 +0530

    feat(spring): ready to implement controller

--- 01 Spring API/math-service/src/main/java/com/servicenow/math/controllers/QuestionController.java
+++ 01 Spring API/math-service/src/main/java/com/servicenow/math/controllers/QuestionController.java
@@ -19,11 +19,9 @@ import java.util.*;
 @RestController
 @RequestMapping("/questions") // <1>
 public class QuestionController {
-    private final RandomQuestionGenerator randomQuestionsGenerator;
     private QuestionsRepository questionsRepository;
-    public QuestionController(QuestionsRepository questionsRepository, RandomQuestionGenerator randomQuestionGenerator) {
+    public QuestionController(QuestionsRepository questionsRepository) {
         this.questionsRepository = questionsRepository;
-        this.randomQuestionsGenerator = randomQuestionGenerator;
     }
 
     @GetMapping("/") // <2>
@@ -31,47 +29,5 @@ public class QuestionController {
         return questionsRepository.getQuestions();
     }
 
-    @GetMapping("/{id}") // <3>
-    public  Question getQuestionById(@PathVariable int id) {
-        return questionsRepository.getQuestionById(id);
-    }
-
-    @PostMapping("/")
-    public Question addQuestion(@Valid @RequestBody Question question) {
-        questionsRepository.addQuestions(List.of(question));
-        return question;
-    }
-
-    @PostMapping("/random")
-    public List<Question> getRequiredNumberOfQuestions(@RequestParam int amount) {
-        try {
-            return questionsRepository.getRequiredNumberOfQuestions(amount);
-        } catch (IllegalArgumentException e) {
-            int currentQuestionCount = questionsRepository.getQuestions().size();
-            var targetXQuestions = amount - currentQuestionCount;
-            List<Question> newQuestions = randomQuestionsGenerator.generateXRandomQuestions(targetXQuestions);
-            questionsRepository.addQuestions(newQuestions);
-            return questionsRepository.getQuestions();
-        }
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResponse handleException(IllegalArgumentException e) {
-        return ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.getMessage());
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException e) {
-        // Create a Map to store field errors
-        Map<String, String> fieldErrors = new HashMap<>();
-
-        // Iterate through field errors and populate the Map
-        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
-            fieldErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-        }
-
-        // Create an HTTP response with the field errors and status code
-        return new ResponseEntity<>(fieldErrors, HttpStatus.BAD_REQUEST);
-    }
 
 }
--- 01 Spring API/math-service/src/main/java/com/servicenow/math/questions/Question.java
+++ 01 Spring API/math-service/src/main/java/com/servicenow/math/questions/Question.java
@@ -7,12 +7,8 @@ import jakarta.validation.constraints.Size;
 import org.springframework.data.annotation.Id;
 
 public record Question(
-        @Id
+
         Integer Id,
-        @NotEmpty(message = "Question must not be empty")
-        @Size(min = 3, max = 255, message = "Question must be between 3 and 255 characters")
         String question,
-        @NotEmpty(message = "Answer must not be empty")
-        @Size(min = 1, max = 255, message = "Answer must be between 1 and 255 characters")
         String answer) {
 }
