c3807f3f7a5caf86411d76392b69fde898d4f66d
petelind <denis@nyu.edu>
2023-09-11 06:18:58 +0530

    feat(spring): add a word on logging and docs

--- 01 Spring API/math-service/src/main/java/com/servicenow/math/controllers/BasicLivenessController.java
+++ 01 Spring API/math-service/src/main/java/com/servicenow/math/controllers/BasicLivenessController.java
@@ -1,11 +1,13 @@
 package com.servicenow.math.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 
 @RestController
+@Slf4j
 @RequestMapping("/")
 public class BasicLivenessController {
 
@@ -17,8 +19,18 @@ public class BasicLivenessController {
 
     @GetMapping("/")
     public String basicAlive()
+    /**
+     * This method is used to check if the service is alive (not healthy or no) without the actuator endpoints.
+     * @param version: The version of the service (e.g. 1.0.0); auto-populated from config.
+     * @param environment: The environment the service is running in (e.g. dev, test, prod); auto-populated from config.
+     * @return String which will contain the version and environment of the service running.
+     * @throws IllegalArgumentException if the version is not a valid version or fails to parse it from config.
+     *
+     */
     {
+        log.info("v." + version + " MathDirect is alive! Environment: " + environment);
         return "v." + version + " MathDirect is alive! Environment: " + environment;
+
     }
 
 
