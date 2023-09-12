9a84be3e8d4d6506f32469c249b050b763e5c084
petelind <denis@nyu.edu>
2023-09-12 08:24:18 +0530

    feat(flux): add a flux example

--- 01 Spring API/exams-service/src/main/java/com/servicenow/exams/controllers/ExamController.java
+++ 01 Spring API/exams-service/src/main/java/com/servicenow/exams/controllers/ExamController.java
@@ -5,52 +5,34 @@ import com.servicenow.exams.exams.ExamRepository;
 import com.servicenow.exams.exams.HistoryClient;
 import com.servicenow.exams.exams.Question;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ResponseStatusException;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/v1")
 public class ExamController {
 
     private final ExamRepository examRepository;
-    private final WebClient webClient;
+    private final WebClient mathClient;
     private final HistoryClient historyClient;
 
-    @Autowired
+
     public ExamController(ExamRepository examRepository, WebClient webClient, HistoryClient historyClient) {
         this.examRepository = examRepository;
-        this.webClient = webClient;
+        this.mathClient = webClient;
         this.historyClient = historyClient;
     }
 
-    @GetMapping("/exams/all")
+    @GetMapping("/exams")
     public List<Exam> getAllExams() {
         return (List<Exam>) examRepository.findAll();
     }
-
-    @PostMapping("/exams/history")
-    @Transactional
-    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
-    // Todo: fix both controllers to accept all required parameters and make them handle exceptions
-    public void createHistoryExam() {
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED, "This endpoint is not implemented yet.");
-    }
-
 
 
     @PostMapping("/exams/math")
@@ -58,7 +40,7 @@ public class ExamController {
     // Todo: fix both controllers to accept all required parameters and make them handle exceptions
     public Mono<Exam> createMathExam() {
         // This is one way of accessing data in another service - via webClient (works for older Spring).
-        return webClient.post().uri("/random?amount=2")
+        return mathClient.post().uri("/random?amount=2")
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .retrieve()
                 .bodyToFlux(Question.class)
@@ -72,13 +54,70 @@ public class ExamController {
                             Exam exam = new Exam();
                             exam.setName("Exam");
                             exam.setQuestions(questionIds);
-                            exam.setDescription("Exam description");
+                            exam.setDescription("Math exam description");
 
                             return examRepository.save(exam);
 
                         }
                 );
     }
+
+    @PostMapping("/exams/history")
+    @Transactional
+    public Mono<Exam> createHistoryExam()
+    {
+        return this.historyClient.getQuestions(2)
+                .map(questions -> {
+                    questions.forEach(System.out::println);
+                    String questionIds = questions.stream()
+                            .map(Question::Id)
+                            .map(String::valueOf) // Convert to string
+                            .collect(Collectors.joining(","));
+
+                    Exam exam = new Exam();
+                    exam.setName("Exam");
+                    exam.setQuestions(questionIds);
+                    exam.setDescription("Exam description");
+
+                    return examRepository.save(exam);
+
+                }
+                );
+    }
+
+    @PostMapping("/exams/complete")
+    @Transactional
+    public Exam createAllSpanningExam(@RequestParam int size, @RequestParam String name, @RequestParam String description) {
+        // get questions from both services
+        Mono<List<Question>> mathQuestions = this.mathClient.post().uri("/random?amount=" + size)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .retrieve()
+                .bodyToFlux(Question.class)
+                .collectList();
+
+        Mono<List<Question>> historyQuestions = this.historyClient.getQuestions(size);
+
+        // combine them into one list
+        Mono<List<Question>> allQuestions = mathQuestions.zipWith(historyQuestions)
+                .map(tuple -> {
+                    List<Question> math = tuple.getT1();
+                    List<Question> history = tuple.getT2();
+                    math.addAll(history);
+                    return math;
+                });
+
+        // TODO: Map the list of questions to a list of question ids. Keep in mind - those are Monos, not Lists!
+        String questionIds = "1, 2, 3, 4";
+
+        Exam exam = new Exam();
+        exam.setName(name);
+        exam.setQuestions(questionIds);
+        exam.setDescription(description);
+        return this.examRepository.save(exam);
+
+    }
+
+
 
     @DeleteMapping("/exams/all")
     public void deleteAllExams() {
