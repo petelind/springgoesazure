5c8eae9add28916a9db0a9bc9e5693c00d16120b
petelind <denis@nyu.edu>
2023-09-15 08:19:33 +0530

    feat(k8s): add exercise for local k8s (liveness hidden)

--- 01 Spring API/build-scripts/kubernetes-local/all-deploy.sh
+++ 01 Spring API/build-scripts/kubernetes-local/all-deploy.sh
@@ -2,4 +2,6 @@
 kubectl create namespace green
 kubectl config set-context --current --namespace=green
 kubectl apply -f deployment.yaml
+wait 5
 kubectl port-forward service/endpoint-examinator 8080:8080
+
